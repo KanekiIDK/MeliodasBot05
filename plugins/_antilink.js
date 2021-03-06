d4badcdbfe4f0f65c38e882f7c52c3bc0b31e7d5
MeliodasJAJA <78351382+MeliodasJAJA@users.noreply.github.com>
2021-03-05 21:27:31 -0700

    Add files via upload

---
+++ plugins/_antilink.js
@@ -0,0 +1,15 @@
+let handler = m => m
+
+let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
+handler.before = m => {
+  if (m.isBaileys && m.fromMe) return true
+  let chat = global.DATABASE.data.chats[m.chat]
+  let isGroupLink = linkRegex.exec(m.text)
+
+  if (chat.antiLink && isGroupLink) m.reply('Elimina!!')
+  
+
+  return true
+}
+
+module.exports = handler
