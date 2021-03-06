3b9412863f1450e5e25228b4fe2e3a47b197e12a
MeliodasJAJA <78351382+MeliodasJAJA@users.noreply.github.com>
2021-03-05 22:36:31 -0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,5 +1,5 @@
 let handler = function (m) {
-  this.sendContact(m.chat, '+529984907794', '👑Samu330👑', m)
+  this.sendContact(m.chat, '+50582733230', '🖤ӄǟռɛӄɨ🔥', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
