755352318d1b56fd08a93ae4659b8574a9553f05
MeliodasJAJA <78351382+MeliodasJAJA@users.noreply.github.com>
2021-03-05 22:50:05 -0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,9 +1,5 @@
 let handler = async m => m.reply(`
-╭─〘 𝑺𝒂𝒎𝒖𝟑𝟑𝟎𝗜 〙
-│ • 𝑃𝑎𝑦𝑝𝑎𝑙 [paypal.me/samu330]
-│ • Pero con que te suscribas a mi canal basta:)
-│ https://www.youtube.com/watch?v=chMc57gjmkI&t=88s
-╰────
+U...?
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
 handler.tags = ['info']
