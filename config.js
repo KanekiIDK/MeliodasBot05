d3d3c32dafed92bd0c63bdadae1e64e4e1110c67
MeliodasJAJA <78351382+MeliodasJAJA@users.noreply.github.com>
2022-06-20 06:55:46 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-global.owner = ['529984907794'] // Put your number here
+global.owner = ['526538490293'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
